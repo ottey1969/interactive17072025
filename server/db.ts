6cc8486a3f74a4286766d84c3ca98bc3180a5126
ottey1969 <otteyfrancisca@gmail.com>
2025-07-18 23:35:14 +0800

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 
-neonConfig.webSocketConstructor = ws;
+// neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
