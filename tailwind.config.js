5b080709737aee572d6f9d7b73f2df167fcd4454
ottey1969 <ottey1969@example.com>
2025-07-19 07:48:50 -0400

    Add tailwind.config.js and add type: module to package.json

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+      },
+    },
+  },
+  plugins: [],
+};
+
